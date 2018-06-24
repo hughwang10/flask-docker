04e19e89072fff30ae67b39204c543da2c55ea06
hughwang10 <hughwang10@gmail.com>
2018-06-24 20:23:46 +0100

    update

--- lifegame/app/main.py
+++ lifegame/app/main.py
@@ -1,13 +1,9 @@
 import os
-from flask import Flask, send_file, render_template, request, jsonify
+from flask import Flask #, send_file, render_template, request, jsonify
 app = Flask(__name__,static_url_path='')
 
 @app.route("/")
 def hello():
-    return "Welcome to docker flask test site!"
-
-@app.route("/lifegame")
-def lifegame():
     return app.send_static_file('index.html')
 
 if __name__ == "__main__":
